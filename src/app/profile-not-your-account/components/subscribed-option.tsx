dc1c3eeb54002d2abca7564973c20760fc27f46e
diko-mahendra <diko.dev99@gmail.com>
2025-03-15 19:31:23 +0700

    feat: subscribption menu

---
+++ src/app/profile-not-your-account/components/subscribed-option.tsx
@@ -0,0 +1,48 @@
+import { SecondaryButton } from "@/components/atoms/secondary-button";
+import Image from "next/image";
+import React from "react";
+
+export default function SubscribedOption({
+  onClose,
+  onClick,
+}: {
+  onClose: () => void;
+  onClick: (type: string) => void;
+}) {
+  return (
+    <div className="bg-white rounded-t-2xl max-w-md mx-auto w-full p-2 shadow-lg">
+      <div className="space-y-2">
+        <button
+          onClick={() => onClick("change-subscription")}
+          className="flex items-center w-full p-4 gap-2 font-medium text-black text-sm border-[#f0f0f0] rounded-2xl border"
+        >
+          <Image alt="icon" width={24} height={24} src="/icons/pencil.svg" />
+          <span>Change subscription</span>
+        </button>
+
+        <button
+          onClick={() => onClick("payment-method")}
+          className="flex items-center gap-2 w-full p-4 font-medium text-black text-sm border-[#f0f0f0] rounded-2xl border"
+        >
+          <Image alt="icon" width={24} height={24} src="/icons/debit.svg" />
+          <span>Edit payment method</span>
+        </button>
+
+        <button
+          onClick={() => onClick("unsubcribe")}
+          className="flex items-center gap-2 w-full p-4 border font-medium text-black text-sm border-[#f0f0f0] rounded-2xl"
+        >
+          <Image alt="icon" width={24} height={24} src="/icons/x.svg" />
+          <span>Unsubscribe</span>
+        </button>
+
+        <SecondaryButton
+          onClick={onClose}
+          className="w-full border-black py-3.5 border-[1.5px] font-medium text-sm mt-4"
+        >
+          Close
+        </SecondaryButton>
+      </div>
+    </div>
+  );
+}
--- src/app/profile-not-your-account/page.tsx
+++ src/app/profile-not-your-account/page.tsx
@@ -14,6 +14,8 @@ import { SubscriptionContent } from "./components/subscribe";
 import ReportUser from "./components/report-user";
 import PopupInfoStatus from "@/components/molecules/popup-info-status";
 import { useToast } from "@/components/molecules/toast";
+import SubscribedOption from "./components/subscribed-option";
+import { useSubscriptionStore } from "@/store/subscription";
 
 export default function Page() {
   const profile: UserProfile = profileData;
@@ -23,9 +25,11 @@ export default function Page() {
     report: false,
     option: false,
     reportSuccess: false,
+    subcribed: false,
   });
   const [isBottomSheetSubscribe, setIsBottomSheetSubscribe] = useState(false);
   const { addToast } = useToast();
+  const { isSubscribed } = useSubscriptionStore();
 
   const tabs = [
     { id: "collections", label: "Collections" },
@@ -48,6 +52,23 @@ export default function Page() {
     }
   };
 
+  const onSubcribed = () => {
+    if (isSubscribed) {
+      setIsBottomSheetOpen((prev) => ({ ...prev, subcribed: true }));
+      setIsBottomSheetSubscribe(false);
+    } else {
+      setIsBottomSheetOpen((prev) => ({ ...prev, subcribed: false }));
+      setIsBottomSheetSubscribe(true);
+    }
+  };
+
+  const onSubscribedMenuOption = (type: string) => {
+    if (type === "change-subscription") {
+      setIsBottomSheetSubscribe(true);
+      setIsBottomSheetOpen((prev) => ({ ...prev, subcribed: false }));
+    }
+  };
+
   return (
     <div className="max-w-md mx-auto bg-white min-h-screen flex flex-col">
       <ProfileHeader
@@ -58,10 +79,7 @@ export default function Page() {
       />
 
       <div className="flex-1 overflow-auto hide-scrollbar mb-24">
-        <ProfileInfo
-          onSubscribe={() => setIsBottomSheetSubscribe(true)}
-          profile={profile}
-        />
+        <ProfileInfo onSubscribe={onSubcribed} profile={profile} />
         <TabNavigation tabs={tabs} defaultActiveTab="collections" />
         <PostsGrid posts={profile.posts} />
       </div>
@@ -123,6 +141,18 @@ export default function Page() {
           onClose={() =>
             setIsBottomSheetOpen((prev) => ({ ...prev, reportSuccess: false }))
           }
+        />
+      </BottomSheet>
+
+      <BottomSheet
+        isOpen={isBottomSheetOpen.subcribed}
+        onClose={() =>
+          setIsBottomSheetOpen((prev) => ({ ...prev, subcribed: false }))
+        }
+      >
+        <SubscribedOption
+          onClick={(type: string) => onSubscribedMenuOption(type)}
+          onClose={() => ({})}
         />
       </BottomSheet>
     </div>
